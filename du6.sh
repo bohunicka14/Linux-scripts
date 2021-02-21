668f8f66ab09523a79ea7cc2665de8057c848171
INKA <bohunicka.ingrid@gmail.com>
2021-02-21 21:24:16 +0100

    added HW descriptions

--- du6.sh
+++ du6.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+/*
+Vytvor script, ktorý bude zobrazovať náhodné príklady. Samozrejme, nech je tam nejaký vstup pre používateľa, aby zadal výsledok. Ak zadá dobrý výsledok, nech vypíše nejaké echo o tom aký je Borec, a ako ho ženy milujú. Ak vypočíta zle, tak tiež nejaké echo. Moc ho nezhadzuj, každému sa to občas stane, ale nech aspoň vidí aký mal byť správny výsledok, aký výsledok zadal on, a o koľko sa ten neštastník pomýlil.
+*/
+
 znamienka=("*" "+" "-" "/")
 random_znamienko_index=$RANDOM%4
 
--- du7.sh
+++ du7.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+/*
+Vieš o tom že existujú aplikácie, ktoré ti zabránia poslať SMS v určitom čase, skôr než vyriešiš nejakú matematickú hádanku? Napríklad v čase od 22:00 do 6:00. Prečo? Lebo je veľká pravdepodobnosť, že budeš pod parou, a na druhý deň by si to mohol ľutovať. Tak teraz sprav niečo podobné a teda, urob script ktorý by mal po spustení poslať nejakú správu. Tú správu user naťuká na vyzvanie. Ale odoslanie sme ešte nepreberali, takže zatiaľ stačí keď tam bude echo s tou správou. Lenže, skôr než to odošleš, tak zisti aký je deň a čas. A ak je piatok alebo sobota v čase od 22:00 do 6:00, tak najskôr zobraz userovi nejakú matematickú hádanku. Napríklad 124*543 (:-D, alebo niečo ľahšie). Ak to vypočíta správne, tak úlohu splnil a možeš poslať správu (teda zobraziť echo). Ak to nevypočíta správne, tak vypíš, že ďalší príklad sa mu zobrazí o 30 sekúnd. Ak ani to nevypočíta správne, ďalší pokus bude mať o 60 sekúnd s tým, že ak ani potom to nevypočíta správne, tak vypíš: "Poslal  som tento text tvojej žene, ty ožraté prasa". A ozaj, tie príklady nech sú random. Preto tá DÚ pred touto. Môže to byť stále sčítavanie, alebo násobilka, len tie čísla nech sú náhodné.
 
+Takže čo potrebuješ? Zistiť a porovnať dátumy. Zistiť vstup od usera pre text správy. Vypísať nejaké príklady a porovnať s očakávaným výsledkom. Nastaviť sleep, if a ďalší read kvôli získaniu výsledku.
+*/
 
 read -p "Zadaj text spravy: " message
 
